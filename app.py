2143361e4ab6a83d193e4a0b0887f65cfc1cec65
vpkumar9661 <vpkumar9661@gmail.com>
2026-03-01 15:41:13 +0530

    added requirements and updated app

--- app.py
+++ app.py
@@ -304,3 +304,9 @@ def logout():
 # ===============================
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
